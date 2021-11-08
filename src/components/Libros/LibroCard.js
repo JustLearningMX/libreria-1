cda30b48a8f0b9f6ed0306910944e5419368bb76
chava <mnchava@outlook.com>
2021-11-07 18:09:48 -0600

    MaterialUI en busqueda de libros

--- src/components/Libros/LibroCard.js
+++ src/components/Libros/LibroCard.js
@@ -1,34 +1,42 @@
 // COMPONENTE QUE DIBUJA UNA TARJETA QUE MUESTRA UNA IMAGEN DE UN LIBRO Y SU TÍTULO
 
 import { Link } from "react-router-dom";
-import styles from "../../css/LibroCard.module.css"; //Estilos para este componente
 import { coverBookIntoLarge } from "../../utils/coverBookIntoLarge";
 import PropTypes from 'prop-types'; //Validación de propiedades
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 export function LibroCard({ libro }) {
-  //Obtenemos un link nuevo con una imagen de mejor calidad
-  const linkCoverBookLarge = coverBookIntoLarge(libro.image_url,"/");
+	//Obtenemos un link nuevo con una imagen de mejor calidad
+	const linkCoverBookLarge = coverBookIntoLarge(libro.image_url, "/");
+	const authorsOfBook = libro.Autores ? libro.Autores.map((autor) => autor.nombre_completo).join(", ") : "";
 
-  //El Link lleva a una página que muestra a detalle un libro
-  //El elemento "li" dibuja una tarjeta por cada libro que recibe
-  return (
-    <Link className={styles.linkCard} to={"/libros/id/" + libro._id}>
-      <li className={styles.libroCard}>
-        <img
-          // width={200}
-          // height={435}
-          className={styles.imgGrid}
-          src={linkCoverBookLarge}
-          alt={libro.title}
-        />
-        <div className={styles.titleContainer}>
-          <p className={styles.title}> {libro.title}</p>
-        </div>
-      </li>
-  </Link>
-  );
+
+	return (
+		<Link to={"/libros/id/" + libro._id}>
+			<Card sx={{ maxWidth: 300 }}>
+				<CardMedia
+					component="img"
+					height="435"
+					image={linkCoverBookLarge}
+					alt={libro.title}
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div">
+						{libro.title}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						{authorsOfBook}
+
+					</Typography>
+				</CardContent>
+			</Card>
+		</Link>
+	);
 }
 
 LibroCard.propTypes = {
-  libro: PropTypes.object.isRequired
+	libro: PropTypes.object.isRequired
 }
